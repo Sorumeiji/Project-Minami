533e1bd3dee0a895f264b22b1090134c27006905
Kaleb Berry <kaleb.berry@outlook.com>
2021-08-29 21:33:18 -0500

    test

--- js/index.js
+++ js/index.js
@@ -1,24 +1,25 @@
-const navSlide = () => {
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.querySelector('.nav__navlist');
-    const navLinks = document.querySelectorAll('.nav__navlist li');
+gsap.from('.sidebar__content', { opacity: 0, duration: 1, x: -50 });
+gsap.from('.featured__content', { opacity: 0, duration: 1, delay: 1, y: -50 });
 
-    // Toogle Nav
-    hamburger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-    });
+// var element = document.querySelector(".sidebar");
+// element.addEventListener("animationstart", listener, false);
+// element.addEventListener("animationend", listener, false);
+// element.addEventListener("animationinteration", listener, false);
 
-    // navLinks.forEach((link, index) => {
-    //     link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s';
-    //     console.log(index / 7);
-    // });
-};
+// element.classclassname = "slidein";
 
-
-window.addEventListener('load', () => {
-    const preload = document.querySelector('.preload');
-    preload.classList.add('preload-finish');
-});
-
-
-navSlide();
+// function listener(event) {
+//     var l = document.createElement("p");
+//     switch (event.type) {
+//         case "animationstart":
+//             l.textContent = `Started: elapsed time is ${event.elapsedTime}`;
+//             break;
+//         case "animationend":
+//             l.textContent = `Ended: elapsed time is ${event.elapsedTime}`;
+//             break;
+//         case "animationiteration":
+//             l.textContent = `New loop started at time ${event.elapsedTime}`;
+//             break;
+//     }
+//     document.querySelector(".sidebar").appendChild(l);
+// }
