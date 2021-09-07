aff46588ebd8b498b3ba839cad94afb9542647d9
Kaleb Berry <kaleb.berry@outlook.com>
2021-09-07 17:55:40 -0500

    updaing mobile navigation

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ gsap.from('.sidebar__content', { opacity: 0, duration: 1, x: -50 });
 gsap.from('.sidebar__footer', { opacity: 0, duration: 1, x: -50 });
 gsap.from('.featured__content', { opacity: 0, duration: 1, delay: 1, y: -50 });
 
+
+
 // When click on commissions navigation the drop-down menu will appear
 const dropdowns = document.getElementsByClassName('main-nav__item');
 
@@ -22,4 +24,10 @@ document.getElementById("main-nav__list").addEventListener("click", function (e)
 });
 
 
+document.getElementById("hamburger").addEventListener("click", function (e) {
 
+    document.querySelector(".main-nav ").classList.toggle("show-flex");
+    // gsap.from('.main-nav, #main-nav__list', { duration: 1, x: -100 });
+    // gsap.from("#main-nav__list", { opacity: 0, duration: 0.5, delay: 0.5, x: -50 });
+
+});
